c36ddc25df893b1db03f9708511c8ae6f8489b47
JonsQu <joonatan.haavisto@cs.tamk.fi>
2019-01-09 11:57:39 +0200

    added database connection

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoClient = require('mongodb').MongoClient;
-
+const mongoose = require('mongoose');
+const plant = require('./routes/plants.route');
 const app = express();
 const URL = 'mongodb://wmp:j00natan@ds133533.mlab.com:33533/watermyplants';
+
+
 //connect to database
-mongoClient.connect(URL, function(err, db){
-    if(err){
-        console.error(err);
-    }else{
-        console.log('We are connected');
-    }
-    db.close();
-})
+mongoose.connect(URL);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
-
+app.use('/plants', plant);
 
 const PORT = process.env.PORT || 3000;
 
