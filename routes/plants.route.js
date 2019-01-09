0b3985817711c9b34669aa7f74b388aac1cb0c7b
JonsQu <joonatan.haavisto@cs.tamk.fi>
2019-01-09 11:59:39 +0200

    added plant route

---
+++ routes/plants.route.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const plant_controller = require('../controllers/plant.controller');
+
+router.post('/create', plant_controller.plant_create);
+
+router.get('/:id', plant_controller.plant_details);
+
+router.post('/search', plant_controller.plant_get_by_name);
+
+router.put('/:id/update', plant_controller.plant_update);
+
+router.delete('/:id/delete', plant_controller.plant_delete);
+
+module.exports = router;
